6e1d87f68c538c3e50fcb66649b02ffbd51b4efe
Talvysh <pecenkasheldan@gmail.com>
2023-04-28 23:57:24 -0500

    Update main.py
    Switching to Pygame instead of Kivy.

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
-from kivy.app import App
-from kivy.uix.widget import Widget
+import pygame
+
+pygame.init()
+
+class App():
+    def __init__(self) -> None:
+        print("Starting up DD...")
+
+        self.window_size = [1280, 720]  # Save to a config later
+        self.window = pygame.display.set_mode(self.window_size)
+        self.running = True
+
+        self.render()
 
 
-class DialogueDesigner(Widget):
-    pass
+    def render(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.quit()
+                
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_ESCAPE:
+                        self.quit()
+
+    
+    def input(self):
+        pass
 
 
-class DDApp(App):
-    def build(self):
-        return DialogueDesigner()
+    def quit(self):
+        print("Doing a clean exit...")
+        self.running = False
+        pygame.quit()
 
 
-if __name__ == "main":
-    DDApp.run()
+app = App()
