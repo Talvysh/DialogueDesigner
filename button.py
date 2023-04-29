3d0748af4bcc6d1ad1a18e3e2470f2d4b7eb508e
Talvysh <pecenkasheldan@gmail.com>
2023-04-29 01:45:45 -0500

    changing local?

--- button.py
+++ button.py
@@ -26,7 +26,4 @@ class Button():
 
         if self.outline_size > 0:
             pygame.draw.rect(surface, self.outline_color, self.rect, self.outline_size)
-    
-    def on_click(self):
-        return self.rect.collidepoint(pygame.mouse.get_pos())
         
